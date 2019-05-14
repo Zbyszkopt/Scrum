e2bf6a7667bc398de8f042bdcdd602f5d47ae804
Krzysztof K <krzykasz.inpg@gmail.com>
2019-05-14 22:10:05 +0200

    Collision -description
    Dodano komentarze w kodzie wyjasniajace funkcjonalnosc implementacji

--- GameEngine/GameEngine/src/Collision.cpp
+++ GameEngine/GameEngine/src/Collision.cpp
@@ -1,16 +1,16 @@
 #include "Collision.h"
 #include "ECS/ColliderComponent.h"
 
-bool Collision::AABB(const SDL_Rect& recA, const SDL_Rect& recB)
+bool Collision::AABB(const SDL_Rect& recA, const SDL_Rect& recB) //opisuje kwadraty w poczatku: lewy gorny rog
 {
 	if (
-		recA.x + recA.w >= recB.x &&
+		recA.x + recA.w >= recB.x && //x,y -wspolrzedne w-szerokosc, h-wysokosc kwadratu -> sprawdza czy warunki kolizji sa spelnione (patrz parametry-x,y,w,h)
 		recB.x + recB.w >= recA.x &&
 		recA.y + recA.h >= recB.y &&
 		recB.y + recB.h >= recA.y
 		)
 	{
-		return true;
+		return true; //true - kwadraty ze soba koliduja
 	}
 
 	return false;
@@ -20,7 +20,8 @@ bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& col
 {
 	if (AABB(colA.collider, colB.collider))
 	{
-		//std::cout << colA.tag << " hit: " << colB.tag << std::endl;
+		//std::cout << colA.tag << " hit: " << colB.tag << std::endl; 
+		//jesli wystepuje kolizja zwraca true i wyswietla hit z parametrami
 		return true;
 	}
 	else
