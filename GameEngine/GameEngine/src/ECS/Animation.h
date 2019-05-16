129029bd0f4c1da4ac9d67d3578bb5f2905642bb
Krzysztof K <krzykasz.inpg@gmail.com>
2019-05-16 20:31:42 +0200

    Animation.h - "simple" struct (end)
    Zakończono wstępny opis struktury animacji - uzupełniono strukturę i opisano.

--- GameEngine/GameEngine/src/ECS/Animation.h
+++ GameEngine/GameEngine/src/ECS/Animation.h
@@ -4,4 +4,13 @@ struct Animation
 	int index; //indeksowanie
 	int frame; //ilosc klatek animacji
 	int speed; //szybkosc animacji
+	
+	Animation() {}
+
+	Animation(int i, int f, int s) //przyjmuje trzy wartosci calkowite: i,f,s - odpowiednio przypisuje je do indeksu, liczby klatek i czasu wyswietlania
+	{
+		index = i;
+		frame = f;
+		speed = s;
+	}
 };
