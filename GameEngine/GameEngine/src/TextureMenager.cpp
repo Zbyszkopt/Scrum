ff4ec4e4f14608af3d3e193af407e8334dca8723
Zbigniew <zzeglecki@gmail.com>
2019-05-16 21:52:56 +0200

    implementacja metody draw klasy texturemenager

--- GameEngine/GameEngine/src/TextureMenager.cpp
+++ GameEngine/GameEngine/src/TextureMenager.cpp
@@ -8,3 +8,8 @@ SDL_Texture* TextureMenager::LoadTexture(const char * texture)
 
 	return tex;
 }
+
+void TextureMenager::Draw(SDL_Texture * tex, SDL_Rect src, SDL_Rect dest, SDL_RendererFlip flip)
+{
+	SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
+}
